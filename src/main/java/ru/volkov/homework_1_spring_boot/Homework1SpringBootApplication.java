c783ec00e579107a6310b991cdf9d9cd2223edd3
rokot108 <38143263+rokot108@users.noreply.github.com>
2019-01-14 16:16:18 +0300

    Initial commit

---
+++ src/main/java/ru/volkov/homework_1_spring_boot/Homework1SpringBootApplication.java
@@ -0,0 +1,14 @@
+package ru.volkov.homework_1_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework1SpringBootApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ru/volkov/homework_1_spring_boot/Homework1SpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.volkov.homework_1_spring_boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Homework1SpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
