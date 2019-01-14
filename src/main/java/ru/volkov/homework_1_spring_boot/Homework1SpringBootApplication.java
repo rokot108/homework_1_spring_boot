74da014c10fde4991c0fd448d5dccd16eb0fcc93
rokot108 <38143263+rokot108@users.noreply.github.com>
2019-01-14 16:24:36 +0300

    Initial_Commit

--- src/main/java/ru/volkov/homework_1_spring_boot/Homework1SpringBootApplication.java
+++ src/main/java/ru/volkov/homework_1_spring_boot/Homework1SpringBootApplication.java
@@ -2,12 +2,20 @@ package ru.volkov.homework_1_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Homework1SpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Homework1SpringBootApplication.class, args);
+    }
+
+    @RequestMapping("/**")
+    public String helloWorld() {
+        return "Hello World";
     }
 
 }
