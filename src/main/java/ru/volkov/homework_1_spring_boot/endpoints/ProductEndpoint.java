99b68ac479a6e1763b1dbe8d696b3e45ca784dae
rokot108 <38143263+rokot108@users.noreply.github.com>
2019-01-24 17:58:35 +0300

    fixes and improvements

--- src/main/java/ru/volkov/homework_1_spring_boot/endpoints/ProductEndpoint.java
+++ src/main/java/ru/volkov/homework_1_spring_boot/endpoints/ProductEndpoint.java
@@ -1,8 +1,8 @@
 package ru.volkov.homework_1_spring_boot.endpoints;
 
-import localhost._8080.products.GetProductDetailsRequest;
-import localhost._8080.products.GetProductDetailsResponse;
-import localhost._8080.products.ProductDetails;
+import org.product.GetProductDetailsRequest;
+import org.product.GetProductDetailsResponse;
+import org.product.ProductDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -19,7 +19,7 @@ public class ProductEndpoint {
 
     @PayloadRoot(namespace = "http://localhost:8080/products", localPart = "GetProductDetailsRequest")
     @ResponsePayload
-    public GetProductDetailsResponse processCourseDetailsRequest(@RequestPayload GetProductDetailsRequest request) {
+    public GetProductDetailsResponse processProductDetailsRequest(@RequestPayload GetProductDetailsRequest request) {
         GetProductDetailsResponse response = new GetProductDetailsResponse();
 
         ProductDetails productDetails = new ProductDetails();
